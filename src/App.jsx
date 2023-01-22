ee8afa57b4cc8f64f5f375b4c54777ccd55dac5b
Kananmas <99276326+Kananmas@users.noreply.github.com>
2023-01-22 16:05:07 +0330

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
-// import { Redirect, Route } from 'react-router-dom';
+//Components
+import { Redirect, Route } from 'react-router-dom';
 import {
+  IonRouterOutlet,
   setupIonicReact
 } from '@ionic/react';
-
+import { IonReactRouter } from '@ionic/react-router';
+import Toggler from './components/Toggler';
+import Slider from './components/Slider';
+//Store
+import { store } from './Store/store';
+//react-redux
+import { Provider } from 'react-redux';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -23,13 +31,20 @@ import '@ionic/react/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
-
 setupIonicReact();
 
 export default function App() {
   return (
-    <div>
-      1
-    </div>
-  );
+    <Provider store={store}>
+      <IonReactRouter>
+        <IonRouterOutlet>
+          <Route path="/dashboard" component={Slider} />
+          <Route path="/Quiz" component={Toggler} />
+          <Redirect exact from='/' to='/dashboard' />
+        </IonRouterOutlet>
+      </IonReactRouter>
+    </Provider>
+  )
 }
+
+export default App;
