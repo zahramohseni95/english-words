bb121c43a268ca594f5da518855a40400b181b52
Kananmas <99276326+Kananmas@users.noreply.github.com>
2023-01-22 23:12:06 +0330

    Update question.list.creator.utilis.js

--- src/utilis/question.list.creator.utilis.js
+++ src/utilis/question.list.creator.utilis.js
@@ -2,9 +2,9 @@ import questionCreator from "./question.creator.utilis";
 
 export default function questionListCreator(list) {
     let questionList = new Array(10);
-
+    let clondedList = [...list]
     for (let i = 0; i < questionList.length; i++) {
-        questionList[i] = questionCreator(list);
+        questionList[i] = questionCreator(clondedList);
     }
 
     return questionList;
