1a9ccd43859bd6c37625fb98be3a7be06f31fbd1
zahra <zmohseni95@gmail.com>
2023-01-22 19:58:22 +0330

    feat: add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,42 @@
-// import { Redirect, Route } from 'react-router-dom';
-import {
-  setupIonicReact
-} from '@ionic/react';
-
+import { Route } from "react-router-dom";
+import { IonApp, IonRouterOutlet, setupIonicReact } from "@ionic/react";
+import { IonReactRouter } from "@ionic/react-router";
 
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
+import "@ionic/react/css/core.css";
 
 /* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
 
 /* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
 
 /* Theme variables */
-import './theme/variables.css';
+import "./theme/variables.css";
 
-
+/* components */
+import { Slider } from "./pages/Slider";
+import { Question } from "./pages/Question";
+import {Result} from "./pages/Result";
 setupIonicReact();
 
 export default function App() {
   return (
-    <div>
-      1
-    </div>
+    <IonApp>
+      <IonReactRouter>
+        <IonRouterOutlet>
+          <Route path="/quiz" component={Question} />
+          <Route exact path="/" component={Slider} />
+          <Route path="/end" component={Result} />
+        </IonRouterOutlet>
+      </IonReactRouter>
+    </IonApp>
   );
 }
