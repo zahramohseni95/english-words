5145ecf4fa3b13ed6296814de3073f974ac01e64
zahra <zmohseni95@gmail.com>
2023-01-23 21:44:06 +0330

    styles: improve slider styles

--- src/components/Slider/index.jsx
+++ src/components/Slider/index.jsx
@@ -1,28 +1,27 @@
 //Components
-import { IonSlides, IonSlide,IonButton } from "@ionic/react"
-import { Link } from "react-router-dom"
+import { IonSlides, IonSlide,IonButton } from "@ionic/react";
 //Styles
 import "./index.css"
 
 export default function Slider() {
     return (
         <IonSlides>
-            <IonSlide className="Slide-section">
+            <IonSlide className="slide-section">
                 <div className="section">
                     <h1>Welcome to GoodVocab</h1>
                     <p>this is a vocabulary app designed for learning english words for all persian speakers</p>
                 </div>
             </IonSlide>
-            <IonSlide className="Slide-section">
+            <IonSlide className="slide-section">
                 <div className="section">
                     <h1>How to use?</h1>
                     <p>in this app, you will take a quiz right after opening the app after finishing quiz you will see your score and your mistakes</p>
                 </div>
             </IonSlide>
-            <IonSlide className="Slide-section">
+            <IonSlide className="slide-section">
                 <div className="section">
                     <h1>Click here to start</h1>
-                     <IonButton color="success" routerLink="/Quiz">Let`s Go!</IonButton>
+                     <IonButton className="section-button" routerLink="/Quiz">Let`s Go!</IonButton>
                 </div>  
             </IonSlide>
         </IonSlides>
