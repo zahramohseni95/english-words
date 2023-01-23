51506230cea65afe96d6e3d18d853a8d7cef20ff
Kananmas <99276326+Kananmas@users.noreply.github.com>
2023-01-23 16:06:30 +0330

    Delete src/Store directory

--- src/Store/store.reducers.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import mistakesReducer from "./quiz/quiz.mistakes.reducer";
-import questionReducer from "./quiz/quiz.selected.question.reducer";
-
-
-export const reducers = combineReducers({
-    mistakes: mistakesReducer,
-    question: questionReducer,
-})
