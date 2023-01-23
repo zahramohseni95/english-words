442eadb4065ec579404f5683ef429b1780d02e26
Kananmas <99276326+Kananmas@users.noreply.github.com>
2023-01-23 17:15:24 +0330

    Update index.jsx

--- src/components/Slider/index.jsx
+++ src/components/Slider/index.jsx
@@ -1,29 +1,31 @@
 //Components
-import { IonSlides, IonSlide,IonButton } from "@ionic/react"
-import { Link } from "react-router-dom"
+import { IonSlides, IonSlide, IonButton, IonText } from "@ionic/react"
 //Styles
 import "./index.css"
+
+//just explain how to use this application
 
 export default function Slider() {
     return (
         <IonSlides>
             <IonSlide className="Slide-section">
-                <div className="section">
+                <IonText >
                     <h1>Welcome to GoodVocab</h1>
-                    <p>this is a vocabulary app designed for learning english words for all persian speakers</p>
-                </div>
+                    <p>This is a vocabulary app designed for learning english words for all persian speakers</p>
+                </IonText>
+
             </IonSlide>
             <IonSlide className="Slide-section">
-                <div className="section">
+                <IonText>
                     <h1>How to use?</h1>
-                    <p>in this app, you will take a quiz right after opening the app after finishing quiz you will see your score and your mistakes</p>
-                </div>
+                    <p>In this app, you will take a quiz right after opening the app after finishing quiz you will see your score and your mistakes</p>
+                </IonText>
             </IonSlide>
             <IonSlide className="Slide-section">
-                <div className="section">
-                    <h1>Click here to start</h1>
-                     <IonButton color="success" routerLink="/Quiz">Let`s Go!</IonButton>
-                </div>  
+                <IonText>
+                    <h1>Are You Ready? </h1>
+                    <IonButton color="success" routerLink="/Quiz">Let`s Go!</IonButton>
+                </IonText>
             </IonSlide>
         </IonSlides>
     )
